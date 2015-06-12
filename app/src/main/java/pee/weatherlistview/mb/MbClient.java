cd8842b846c0ec0130cf1f159653a3e61bc336cb
maivo <vupt@yahoo.com>
2015-06-12 15:18:52 -0600

    initial_add

---
+++ app/src/main/java/pee/weatherlistview/mb/MbClient.java
@@ -0,0 +1,83 @@
+package pee.weatherlistview.mb;
+
+import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.ResponseHandlerInterface;
+import com.x5.template.Chunk;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.StringEntity;
+
+import pee.weatherlistview.ui.AppSession;
+import pee.weatherlistview.util.AssetUtils;
+
+/**
+ * Created by pvu_asus on 28/05/2015.
+ */
+public class MbClient {
+    private static final String TAG = "MbClient";
+
+    private static final String BASE_URL ="http://172.20.10.4:8080/mbmock";
+
+
+
+    public static String getSsGetMultifactorSecurityInfoRequestXml(Context context, String userName, String password){
+        String assetPath = "mb_securityService/securityService_getMultifactorSecurityInfo_request.cxml";
+        String chunkTemplateContent = AssetUtils.getAssetContent(context, assetPath);
+
+        //get result
+        Chunk c = new Chunk();
+        c.append(chunkTemplateContent);
+        c.set("userName", userName);
+        c.set("password", password);
+        return c.toString();
+    }
+
+    public static String getSsAuthenticateUserRequestXml(Context context, String userName, String password){
+        String assetPath = "mb_securityService/securityService_authenticateUser_request.cxml";
+        String chunkTemplateContent = AssetUtils.getAssetContent(context, assetPath);
+        //get result
+        Chunk c = new Chunk();
+        c.append(chunkTemplateContent);
+        c.set("userName", userName);
+        c.set("password", password);
+        return c.toString();
+    }
+
+    public static String getAsGetAccountsRequestXml(Context context, AppSession appSession){
+        String assetPath = "mb_accountService/accountService_getAccounts_request.cxml";
+        String chunkTemplateContent = AssetUtils.getAssetContent(context, assetPath);
+        //get result
+        Chunk c = new Chunk();
+        c.append(chunkTemplateContent);
+        c.set("userName", appSession.getUserName());
+        c.set("password", appSession.getPassword());
+        c.set("channelSessionId", appSession.getChannelSessionId());
+        return c.toString();
+    }
+
+
+
+    public static void post(Context context, String endPoint, String requestXml, ResponseHandlerInterface responseHandler){
+        AsyncHttpClient client = new AsyncHttpClient();
+        HttpEntity entity;
+        try {
+            entity = new StringEntity(requestXml);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        String  contentType = "text/xml; charset=utf-8";
+
+
+        client.addHeader("Content-Type", contentType);
+        // client.addHeader("Accept", "text/xml");
+        client.addHeader("SOAPAction", endPoint);
+        client.post(context, endPoint, entity, contentType, responseHandler);
+
+    }
+
+
+
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+codeproject weather listview
+
+http://www.codeproject.com/Articles/507651/Customized-Android-ListView-with-Image-and-Text
